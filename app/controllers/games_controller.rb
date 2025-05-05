4d9845e0d543c2b40e90e986686b075409193a45
Sbeltran16 <sbeltran16@gmail.com>
2025-05-05 00:40:05 -0700

    changes to the games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -16,7 +16,15 @@ class GamesController < ApplicationController
     request["Client-ID"] = CLIENT_ID
     request["Authorization"] = "bearer #{token}"
     request["Content-Type"] = "text/plain"
-    request.body = "fields name, rating; sort rating desc; limit 10;"
+    request.body = <<~BODY
+    fields name, total_rating_count, cover.image_id, artworks.image_id, updated_at, summary, release_dates, rating, genres.name;
+    where first_release_date > #{(Time.now - 14.days).to_i};
+    sort rating asc;
+    limit 6;
+  BODY
+
+
+
 
     response = http.request(request)
 
@@ -33,24 +41,22 @@ class GamesController < ApplicationController
   private
   def fetch_access_token
     uri = URI("https://id.twitch.tv/oauth2/token")
-    
+
     response = Net::HTTP.post_form(uri, {
       client_id: ENV["TWITCH_CLIENT_ID"],
       client_secret: ENV["TWITCH_CLIENT_SECRET"],
       grant_type: "client_credentials"
     })
-  
+
     Rails.logger.debug("Twitch Token Response: #{response.body}")
-  
+
     response_data = JSON.parse(response.body)
-  
+
     Rails.logger.debug("Twitch Token Parsed Response: #{response_data}")
-  
+
     access_token = response_data["access_token"]
     Rails.logger.debug("Twitch Token: #{access_token}")
-  
+
     access_token
   end
-  
-  
 end
