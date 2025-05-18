debc2797808daf4bb4d9876157d3cabaf5453a6f
Sbeltran16 <sbeltran16@gmail.com>
2025-05-18 04:43:47 -0700

    Small code cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Current User
-   get "/me", to: "users#me"
+  get "/me", to: "users#me"
 
   #User routes
 
