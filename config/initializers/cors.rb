899c821627fbb8af1ab23c77f953f984cf85fe96
Sbeltran16 <sbeltran16@gmail.com>
2025-05-23 06:31:06 -0700

    changes to allowed url in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://press-start-beta.vercel.app", "pressstart.gg"
+    origins "http://localhost:3000", "https://press-start-beta.vercel.app", "http://pressstart.gg"
 
     resource "*",
       headers: :any,
