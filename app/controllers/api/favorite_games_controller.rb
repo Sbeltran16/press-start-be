898a662919496e3452622897f6034c8e2302d74c
Sbeltran16 <sbeltran16@gmail.com>
2025-06-09 06:18:49 -0700

    New logic to favorite games index.

--- app/controllers/api/favorite_games_controller.rb
+++ app/controllers/api/favorite_games_controller.rb
@@ -3,7 +3,14 @@ class Api::FavoriteGamesController < ApplicationController
 
 
   def index
-    render json: current_user.favorite_games.pluck(:igdb_game_id)
+    if params[:username]
+      user = User.find_by(username: params[:username])
+      return render json: { error: "User not found"}, status: :not_found unless user
+
+      render json: user.favorite_games.pluck(:igdb_game_id)
+    else
+      render json: current_user.favorite_games.pluck(:igdb_game_id)
+    end
   end
 
   def update
@@ -11,16 +18,15 @@ class Api::FavoriteGamesController < ApplicationController
     if igdb_ids.is_a?(Array) && igdb_ids.size <= 4
       # Remove all old favorites
       current_user.favorite_games.destroy_all
-  
+
       # Create new favorites with IGDB IDs
       igdb_ids.each do |igdb_id|
         current_user.favorite_games.create!(igdb_game_id: igdb_id)
       end
-  
+
       render json: { message: "Favorites updated" }, status: :ok
     else
       render json: { error: "Invalid favorite games list" }, status: :unprocessable_entity
     end
   end
-  
 end
