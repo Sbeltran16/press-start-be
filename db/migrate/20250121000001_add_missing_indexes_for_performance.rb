c960af9671bd5c98175bbc23f2641dd8902f4d21
Sbeltran16 <sbeltran16@gmail.com>
2026-01-05 10:37:56 -0800

    Fixes

--- db/migrate/20250121000001_add_missing_indexes_for_performance.rb
+++ db/migrate/20250121000001_add_missing_indexes_for_performance.rb
@@ -3,18 +3,39 @@ class AddMissingIndexesForPerformance < ActiveRecord::Migration[7.1]
     # Add indexes on igdb_game_id columns for better query performance
     # These are used frequently in GameScoreService and other queries
     
-    add_index :game_views, :igdb_game_id, unless index_exists?(:game_views, :igdb_game_id)
-    add_index :game_likes, :igdb_game_id, unless index_exists?(:game_likes, :igdb_game_id)
-    add_index :game_plays, :igdb_game_id, unless index_exists?(:game_plays, :igdb_game_id)
-    add_index :reviews, :igdb_game_id, unless index_exists?(:reviews, :igdb_game_id)
-    add_index :ratings, :igdb_game_id, unless index_exists?(:ratings, :igdb_game_id)
+    unless index_exists?(:game_views, :igdb_game_id)
+      add_index :game_views, :igdb_game_id
+    end
+    
+    unless index_exists?(:game_likes, :igdb_game_id)
+      add_index :game_likes, :igdb_game_id
+    end
+    
+    unless index_exists?(:game_plays, :igdb_game_id)
+      add_index :game_plays, :igdb_game_id
+    end
+    
+    unless index_exists?(:reviews, :igdb_game_id)
+      add_index :reviews, :igdb_game_id
+    end
+    
+    unless index_exists?(:ratings, :igdb_game_id)
+      add_index :ratings, :igdb_game_id
+    end
     
     # Add index on created_at for time-based queries
-    add_index :reviews, :created_at, unless index_exists?(:reviews, :created_at)
-    add_index :game_lists, :created_at, unless index_exists?(:game_lists, :created_at)
+    unless index_exists?(:reviews, :created_at)
+      add_index :reviews, :created_at
+    end
+    
+    unless index_exists?(:game_lists, :created_at)
+      add_index :game_lists, :created_at
+    end
     
     # Add index on follows for better follower/following queries
-    add_index :follows, :followed_id, unless index_exists?(:follows, :followed_id)
+    unless index_exists?(:follows, :followed_id)
+      add_index :follows, :followed_id
+    end
   end
 end
 
